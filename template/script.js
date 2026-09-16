e2bfef2d5e3a655d1aef0e7637028eb8ace3fcd9
b4ssem <bmeghiche06@gmail.com>
2026-09-16 14:19:04 +0200

    partie 2

--- template/script.js
+++ template/script.js
@@ -1,4 +1,7 @@
 const tplRaw = document.querySelector("[data-template-id='hello-message']");
 const tpl = tplRaw.content.cloneNode(true);
-const targetDiv = document.querySelector(".Titre");
-targetDiv.append(tpl);
+
+document.querySelector(".Titre").append(tpl);
+document.querySelector("[data-template-id='mess']").textContent = "Texte changé";
+document.querySelector("[data-template-id='mess']").classList.add("test", "test1");
+document.querySelector("[data-template-id='image1']").setAttribute("src", "https://avatars.githubusercontent.com/u/188966379?v=4");
